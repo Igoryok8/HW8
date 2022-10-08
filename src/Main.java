1c285f29868051d9c408075d132386ff000c8ba6
Igoryok8 <energymag@mail.ru>
2022-10-09 02:10:00 +0300

    Задание 1 и 2 выполнены

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,30 @@ public class Main {
 
         System.out.println("Домашнее задание 1");
         System.out.println("Задание 1");
+        int deposit = 15000;
+        double percent = 12;
+        double annualRatet = percent / 100;
+        double monthPercent = annualRatet / 12;
+        double sum = 0;
+        while (sum <= 2_459_000) {
+            sum = (sum + deposit) * (1 + monthPercent);
+            //  System.out.println("Месяц " + i + " ,сумма накоплений равна " + sum + " рублей");
+            System.out.println(sum);
+        }
+        //System.out.println(sum);
+        //System.out.println(i);
+        System.out.println("Задание 2");
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (i = 10; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
     }
 }
